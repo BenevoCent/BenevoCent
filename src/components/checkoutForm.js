f9d0fa0a5bb6b74d1664c5b0ac41b50e804aa585
devere-here <stevendeverehere@gmail.com>
2018-03-26 10:51:09 -0400

    stashing changes to get new changes to master

--- src/components/checkoutForm.js
+++ src/components/checkoutForm.js
@@ -6,7 +6,7 @@ import RaisedButton from "material-ui/RaisedButton";
 
 import { db } from "../config/constants";
 import {Elements} from 'react-stripe-elements';
-import {injectStripe} from 'react-stripe-elements';
+import {Stripe, injectStripe, stripeTokenHandler} from 'react-stripe-elements';
 import CardSection from './cardSection'
 
 let formStyle = {
@@ -14,7 +14,6 @@ let formStyle = {
     width: "400px",
     border: "1px black solid"
 }
-
 
 
 export class CheckoutForm extends Component {
@@ -41,12 +40,69 @@ export class CheckoutForm extends Component {
   render() {
     //let userId = this.props.user.uid;
     //const userAccounts = this.state.userAccounts;
+    var stripe = Stripe('pk_test_7NDxNFwTXZI5iGsCursLGPh2');
+
+    // Create an instance of Elements.
+    var elements = stripe.elements();
+
+    // Custom styling can be passed to options when creating an Element.
+    // (Note that this demo uses a wider set of styles than the guide below.)
+    var style = {
+    base: {
+        color: '#32325d',
+        lineHeight: '18px',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+        color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: '#fa755a',
+        iconColor: '#fa755a'
+    }
+    };
+
+    // Create an instance of the card Element.
+    var card = elements.create('card', {style: style});
+
+    // Add an instance of the card Element into the `card-element` <div>.
+    card.mount('#card-element');
+
+    // Handle real-time validation errors from the card Element.
+    card.addEventListener('change', function(event) {
+        var displayError = document.getElementById('card-errors');
+        if (event.error) {
+        displayError.textContent = event.error.message;
+        } else {
+        displayError.textContent = '';
+        }
+    });
+  
+    // Handle form submission.
+    var form = document.getElementById('payment-form');
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+    
+        stripe.createToken(card).then(function(result) {
+        if (result.error) {
+            // Inform the user if there was an error.
+            var errorElement = document.getElementById('card-errors');
+            errorElement.textContent = result.error.message;
+        } else {
+            // Send the token to your server.
+            stripeTokenHandler(result.token);
+        }
+        });
+    });
+
     return (
         <div style={formStyle}>
             <form action="/charge" method="post" id="payment-form">
                 <div class="form-row">
-                    <label for="card-element"></label>
-                    <div id="card-element"></div>
+                    <label for="card-element">Credit or Debit Card</label>
+                    <div id="card-element">{/*put stripe element here*/}</div>
                     <div id="card-errors" role="alert"></div>
                 </div>
                 <button>Submit Payment</button>
