aaf6ab6053ddb0c7b741c200c47bf132da34d7f2
Michael Sam <michaelhongsam@gmail.com>
2018-03-26 23:00:35 -0400

    fix: test specs

--- src/components/Charities.js
+++ src/components/Charities.js
@@ -68,18 +68,18 @@ export default class Charities extends Component {
       .get()
       .then(doc => doc.data())
       .then(selectedCharities => {
-        this.setState({selectedCharities: selectedCharities})
+        this.setState({ selectedCharities: selectedCharities })
         return Promise.all(
           Object.keys(selectedCharities)
-          .map(
-            key => db.collection('charities')
-              .doc(key)
-              .get()
-              .then(doc => doc.data().name)
-              .then(name => ({
-                [name]: selectedCharities[key]
-              }))
-          )
+            .map(
+              key => db.collection('charities')
+                .doc(key)
+                .get()
+                .then(doc => doc.data().name)
+                .then(name => ({
+                  [name]: selectedCharities[key]
+                }))
+            )
         )
       })
       .then(entries => Object.assign(...entries))
@@ -133,99 +133,99 @@ export default class Charities extends Component {
             />
             <GridList style={styles.gridList} cols={2}>
               {
-              updatedCharities.map(charity => {
-                return (
-                  <GridTile
-                    key={charity.name}
-                    title={charity.name}
-                    subtitle={
-                      <span>
-                        <b>{charity.tag}</b>
-                      </span>
-                    }
-                    actionIcon={
-                      this.state.namedCharities.hasOwnProperty(charity.name) ? (
-                        <IconButton
-                          onClick={() => {
-                            console.log('clicked: ', charity.name, charity.uid)
+                updatedCharities.map(charity => {
+                  return (
+                    <GridTile
+                      key={charity.name}
+                      title={charity.name}
+                      subtitle={
+                        <span>
+                          <b>{charity.tag}</b>
+                        </span>
+                      }
+                      actionIcon={
+                        this.state.namedCharities.hasOwnProperty(charity.name) ? (
+                          <IconButton
+                            onClick={() => {
+                              console.log('clicked: ', charity.name, charity.uid)
 
-                            let newNamedCharities = this.state.namedCharities;
-                            delete newNamedCharities[charity.name]
-                            this.setState({namedCharities: newNamedCharities})
+                              let newNamedCharities = this.state.namedCharities;
+                              delete newNamedCharities[charity.name]
+                              this.setState({ namedCharities: newNamedCharities })
 
-                            let newSelectedCharities = this.state.selectedCharities;
-                            delete newSelectedCharities[charity.uid]
-                            this.setState({selectedCharities: newSelectedCharities})
+                              let newSelectedCharities = this.state.selectedCharities;
+                              delete newSelectedCharities[charity.uid]
+                              this.setState({ selectedCharities: newSelectedCharities })
 
-                            console.log("this.state", this.state)
+                              // console.log("this.state", this.state)
 
-                            db
-                              .collection('distributions')
-                              .doc(this.props.user.uid)
-                              .set(
-                                this.state.selectedCharities,
+                              db
+                                .collection('distributions')
+                                .doc(this.props.user.uid)
+                                .set(
+                                  this.state.selectedCharities,
                               )
-                              .then(function() {
-                                console.log('Document successfully written!');
-                              })
-                              .catch(function(error) {
-                                console.error('Error writing document: ', error);
-                              });
-                          }}
-                        >
-                          <CheckBox color="rgb(255, 255, 255)" />
-                        </IconButton>
-                      ) : (
-                        <IconButton
-                          onClick={() => {
-                            console.log('clicked: ', charity.name, charity.uid)
+                                .then(function () {
+                                  console.log('Document successfully written!');
+                                })
+                                .catch(function (error) {
+                                  console.error('Error writing document: ', error);
+                                });
+                            }}
+                          >
+                            <CheckBox color="rgb(255, 255, 255)" />
+                          </IconButton>
+                        ) : (
+                            <IconButton
+                              onClick={() => {
+                                console.log('clicked: ', charity.name, charity.uid)
 
-                            let newNamedCharities = this.state.namedCharities;
-                            newNamedCharities[charity.name] = 0;
-                            this.setState({namedCharities: newNamedCharities})
+                                let newNamedCharities = this.state.namedCharities;
+                                newNamedCharities[charity.name] = 0;
+                                this.setState({ namedCharities: newNamedCharities })
 
-                            let newSelectedCharities = this.state.selectedCharities;
-                            newSelectedCharities[charity.uid] = 0;
-                            this.setState({selectedCharities: newSelectedCharities})
+                                let newSelectedCharities = this.state.selectedCharities;
+                                newSelectedCharities[charity.uid] = 0;
+                                this.setState({ selectedCharities: newSelectedCharities })
 
-                            // console.log("this.state", this.state)
+                                // console.log("this.state", this.state)
 
-                            db
-                              .collection('distributions')
-                              .doc(this.props.user.uid)
-                              .set(
-                                this.state.selectedCharities,
-                                // { merge: true }
-                              )
-                              .then(function() {
-                                console.log('Document successfully written!');
-                              })
-                              .catch(function(error) {
-                                console.error('Error writing document: ', error);
-                              });
-                          }}
-                        >
-                          <CheckBoxOutlineBlank color="rgb(255, 255, 255)" />
-                        </IconButton>
-                      )
-                    }
-                    style={{
-                      display: 'flex',
-                      justifyContent: 'center',
-                      alignItems: 'center'
-                      // flexDirection: 'column',
-                    }}
-                    titleStyle={styles.titleStyle}
-                    titleBackground="linear-gradient(to top, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)"
-                  >
-                    <img src={charity.img} alt="charity" />
-                    <br />
-                    <a href={charity.url} target="_blank">
-                      Main Website
+                                db
+                                  .collection('distributions')
+                                  .doc(this.props.user.uid)
+                                  .set(
+                                    this.state.selectedCharities,
+                                  // { merge: true }
+                                )
+                                  .then(function () {
+                                    console.log('Document successfully written!');
+                                  })
+                                  .catch(function (error) {
+                                    console.error('Error writing document: ', error);
+                                  });
+                              }}
+                            >
+                              <CheckBoxOutlineBlank color="rgb(255, 255, 255)" />
+                            </IconButton>
+                          )
+                      }
+                      style={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center'
+                        // flexDirection: 'column',
+                      }}
+                      titleStyle={styles.titleStyle}
+                      titleBackground="linear-gradient(to top, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)"
+                    >
+                      <img src={charity.img} alt="charity" />
+                      <br />
+                      <a href={charity.url} target="_blank">
+                        Main Website
                     </a>
-                  </GridTile>
-                );
-              })}
+                    </GridTile>
+                  );
+                })}
             </GridList>
           </div>
           <div style={styles.root}>
